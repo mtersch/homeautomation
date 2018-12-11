a54e6c2ef7d638b0f7af0f50830773858d218525
Martijn ter Schegget <27376204+mtersch@users.noreply.github.com>
2018-12-11 13:19:41 +0100

    One DimmableLightbulb coming your way!

--- src/nl/saxion/mbi12/examples/oop1819/week4/homeautomation/model/DimmableLightbulb.java
+++ src/nl/saxion/mbi12/examples/oop1819/week4/homeautomation/model/DimmableLightbulb.java
@@ -1,10 +1,18 @@
 package nl.saxion.mbi12.examples.oop1819.week4.homeautomation.model;
 
 public class DimmableLightbulb extends Lightbulb {
+    private int percentage;
+
+    public DimmableLightbulb() {
+        super();
+        percentage = 0;
+    }
+
     /**
      * Set brightness as a percentage of maximum brightness
      * @param percentage Brightness percentage to set (value in range 0-100)
      */
     public void setBrightness(int percentage) {
+        this.percentage = percentage;
     }
 }
