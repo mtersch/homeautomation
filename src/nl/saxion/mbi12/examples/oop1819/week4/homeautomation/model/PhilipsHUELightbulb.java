a1a288cf281a5d29d4bfa9509dd5f9b528467f10
Martijn ter Schegget <27376204+mtersch@users.noreply.github.com>
2018-12-11 12:06:41 +0100

    Readability changes: parameters named properly, JavaDoc added.

--- src/nl/saxion/mbi12/examples/oop1819/week4/homeautomation/model/PhilipsHUELightbulb.java
+++ src/nl/saxion/mbi12/examples/oop1819/week4/homeautomation/model/PhilipsHUELightbulb.java
@@ -1,6 +1,12 @@
 package nl.saxion.mbi12.examples.oop1819.week4.homeautomation.model;
 
 public class PhilipsHUELightbulb extends DimmableLightbulb {
-    public void setColor(int i, int i1, int i2) {
+    /**
+     * Set the color of this HUE bulb to a color specified as red/green/blue combination
+     * @param red Value for red component of light (value in range 0-255)
+     * @param green Value for green component of light (value in range 0-255)
+     * @param blue Value for blue component of light (value in range 0-255)
+     */
+    public void setColor(int red, int green, int blue) {
     }
 }
