92627c7d3167f81499eabe89cc529599f2b88a76
Martijn ter Schegget <27376204+mtersch@users.noreply.github.com>
2018-12-11 13:15:25 +0100

    Implemented Lightbulb.turnOn() (and constructors for Lightbulb).

--- src/nl/saxion/mbi12/examples/oop1819/week4/homeautomation/model/Lightbulb.java
+++ src/nl/saxion/mbi12/examples/oop1819/week4/homeautomation/model/Lightbulb.java
@@ -1,9 +1,19 @@
 package nl.saxion.mbi12.examples.oop1819.week4.homeautomation.model;
 
 public class Lightbulb {
+    private boolean isOn;
+
+    public Lightbulb() {
+        this(false);
+    }
+    public Lightbulb(boolean isOn) {
+        this.isOn = isOn;
+    }
+
     /**
      * Turn on the lightbulb (change state from 'off' to 'on'; does nothing if state already is 'on')
      */
     public void turnOn() {
+        isOn = true;
     }
 }
